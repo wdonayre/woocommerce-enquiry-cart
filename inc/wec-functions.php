12a4929b804c07fe5619c7bb90a6a85a67e55e66
William Donayre Jr <wdonayredroid@gmail.com>
2018-09-12 12:51:48 +0800

    add woocommerce hooks

--- inc/wec-functions.php
+++ inc/wec-functions.php
@@ -1,3 +1,12 @@
 <?php
 
+function add_to_enquiry_trigger_func(){
+    echo '<div class="wec-trigger-outer"><button class="wec-btn wec-add-trigger">Add to Enquiry</button></div>';
+}
+//add_action( 'woocommerce_product_thumbnails', 'add_to_enquiry_trigger_func', 15 );
 
+//woocommerce_after_shop_loop_item_title
+function add_to_enquiry_trigger_in_loop_func(){
+    echo '<div class="wec-trigger-outer"><button class="wec-btn wec-add-trigger">Add to Enquiry</button> <button class="wec-btn wec-learn-more">Learn More</button> </div>';
+}
+add_action( 'woocommerce_after_shop_loop_item_title', 'add_to_enquiry_trigger_in_loop_func', 15 );
